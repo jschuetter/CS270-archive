f2a3f411093ec1ae63c370389dc6af6946c5e8ed
jschuetter <jmbschuetter@gmail.com>
2024-01-25 13:13:42 -0500

    Minor bug fixes: changed keyPtr backup to use strdup() to copy value, not address

--- cs270proj0/strmap.c
+++ cs270proj0/strmap.c
@@ -3,7 +3,7 @@
 //Hash function for (deterministically) generating location in map based on key value
 //Takes number of buckets in hashmap and to hash
 //Returns index in hash map for given key
-int hash (unsigned int numbuckets, char* key) {
+static int hash (unsigned int numbuckets, char* key) {
 	unsigned long rv = 12421;  //Initialize return value to prime number over MAX_BUCKETS
 	char curChar = key[0];
 	int keyInd = 0;
@@ -16,25 +16,15 @@ int hash (unsigned int numbuckets, char* key) {
 	return rv%(numbuckets);
 }
 
-//Check malloc returns - returns true if valid, exits if not
-void retcheck(void *ptr) {
-	if (ptr == NULL) {
-		perror("malloc");
-		exit(0);
-	}
-}
-
 strmap_t *strmap_create (int numbuckets) {
 	//Handle numbuckets values out of range defined in strmap.h
 	if (numbuckets > MAX_BUCKETS) numbuckets = MAX_BUCKETS;
 	else if (numbuckets < MIN_BUCKETS) numbuckets = MIN_BUCKETS;
 
 	strmap_t *rv = (strmap_t *) malloc(sizeof(strmap_t));
-	retcheck(rv);
 	rv->strmap_size = 0;
 	rv->strmap_nbuckets = numbuckets;
 	rv->strmap_buckets =  (smel_t **) calloc(numbuckets,sizeof(void *)); //Initialize bucket array according to numbuckets argument
-	retcheck(rv->strmap_buckets);
 
 	return rv;
 }
@@ -44,8 +34,8 @@ void *strmap_put(strmap_t *m, char *key, void *value) {
 	void *rv = NULL;
 
 	//Allocate memory for key string to avoid dereference error
-	char *keyPtr = (char *) malloc(sizeof(key));
-	keyPtr = key;
+	char *keyPtr = strdup(key);//(char *) malloc(sizeof(key));
+//	*keyPtr = strdup(key);
 	printf("Saved key to heap\n");
 
 	//Check for previous value at given key - move to within 
